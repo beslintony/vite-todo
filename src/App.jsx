20eedaba952d744d62cd6b1d5130e1fb4a874384
beslintony <83027084+beslintony@users.noreply.github.com>
2023-10-18 10:39:45 +0200

    feat. add persit todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { Container, Paper } from "@mantine/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(storedTodos);
+    setLoaded(true); // Set loaded to true after data is retrieved
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  }, [todos, loaded]);
 
   const addTodo = (newTodo) => {
     setTodos([...todos, newTodo]);
@@ -24,7 +38,7 @@ function App() {
 
   return (
     <Container size={500} style={{ marginTop: "2rem" }}>
-      <Paper padding="md" shadow="xs" radius="md">
+      <Paper shadow="xs" p="xl" radius="md">
         <TodoInput addTodo={addTodo} />
         <TodoList
           todos={todos}
